a5dea3785c29f7f9a5f732e6a1e923f066bdaf68
LAPTOP-052BP8GG\ASUS <giang.nguyengiang@hcmut.edu.vn>
2024-09-23 11:34:37 +0700

    INIT FOR EX2

--- LAB2_TIMER/Core/Inc/main.h
+++ LAB2_TIMER/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
 #define LED_RED_Pin GPIO_PIN_5
 #define LED_RED_GPIO_Port GPIOA
 #define EN0_Pin GPIO_PIN_6
@@ -70,6 +72,10 @@ void Error_Handler(void);
 #define LED_B_GPIO_Port GPIOB
 #define LED_C_Pin GPIO_PIN_2
 #define LED_C_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
 #define LED_D_Pin GPIO_PIN_3
 #define LED_D_GPIO_Port GPIOB
 #define LED_E_Pin GPIO_PIN_4
